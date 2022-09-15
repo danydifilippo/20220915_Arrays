8c956305f24096a36c9bf0e7c150e3b9220056fc
danydifilippo <danydifilippo@gmail.com>
2022-09-15 15:49:35 +0200

    initial

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* Punto 1: metodi maiuscolo e minuscolo, 
+suddivisione nei suoi elementi,
+selezionarne 2 */
+
+let beginLesson = "Buongiorno ragazzi, benvenuti alla prima lezione di inglese";
+
+let upper_ = beginLesson.toUpperCase();
+console.log(upper_);
